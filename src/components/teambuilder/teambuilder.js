a3eb96f12d2d352d5ce67c9c9834168b00e31471
QQChowey <quantang98@gmail.com>
2019-12-19 10:32:39 -0800

    first test

---
+++ src/components/teambuilder/teambuilder.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './teambuilder.module.css'
+
+const TeamBuilder = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.box} />
+        </div>
+    )
+}
+
+export default TeamBuilder

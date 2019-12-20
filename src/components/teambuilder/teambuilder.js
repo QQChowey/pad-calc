56e97e33c4979b973fcde6630500c8212f04c66d
QQChowey <quantang98@gmail.com>
2019-12-20 15:48:01 -0800

    incomplete v2 - layout finished

--- src/components/teambuilder/teambuilder.js
+++ src/components/teambuilder/teambuilder.js
@@ -10,12 +10,13 @@ class TeamBuilder extends Component {
     render() {
         return (
             <div className={styles.container}>
-                <div className={styles.box1}>
-                    <Dropdown value={["1P", "2P", "3P"]}/>
-                </div>
-                <div className={styles.box2}>
+                <Dropdown value={["1P", "2P", "3P"]}/>
+                <div className={styles.spacer} />                
+                <div className={styles.box}>
                     <TeamTable />
+                    <div className={styles.spacer} />
                     <TeamTable />
+                    <div className={styles.spacer} />
                     <TeamTable />
                 </div>
             </div>
